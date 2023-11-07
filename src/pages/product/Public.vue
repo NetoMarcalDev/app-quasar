607165af4b4cfed9261ccbbdad8ee3a84c9aff35
Neto Marçal <neto.marcal.dev@gmail.com>
2023-11-07 17:04:57 -0300

    Implementação do Detalhando do Produto

--- src/pages/product/Public.vue
+++ src/pages/product/Public.vue
@@ -1,36 +1,43 @@
 <template>
-  <div class="q-pa-md">
-    <q-table
-      grid
-      :rows="products"
-      :columns="columnsProduct"
-      row-key="id"
-      :loading="loading"
-      :filter="filter"
-    >
-      <template v-slot:top>
-        <span class="text-h6"> Produtos </span>
-        <q-space />
-        <q-input outlined dense debounce="300" v-model="filter" placeholder="Consultar" class="q-mr-sm">
-          <template v-slot:append>
-            <q-icon name="mdi-magnify" />
-          </template>
-        </q-input>
-      </template>
+  <q-page padding>
+    <div class="q-pa-md">
+      <q-table
+        grid
+        :rows="products"
+        :columns="columnsProduct"
+        row-key="id"
+        :loading="loading"
+        :filter="filter"
+      >
+        <template v-slot:top>
+          <span class="text-h6"> Produtos </span>
+          <q-space />
+          <q-input outlined dense debounce="300" v-model="filter" placeholder="Consultar" class="q-mr-sm">
+            <template v-slot:append>
+              <q-icon name="mdi-magnify" />
+            </template>
+          </q-input>
+        </template>
 
-      <template v-slot:item="props">
-        <div class="q-pa-xs col-xs-12 col-sm-6 col-md-4">
-          <q-card flat bordered>
-            <q-img :src="props.row.img_url" :ratio="4/3" />
-            <q-card-section class="text-center">
-              <div class="text-h6">{{ props.row.name }}</div>
-              <div class="text-subtitle2">{{ formatCurrency(props.row.price) }}</div>
-            </q-card-section>
-          </q-card>
-        </div>
-      </template>
-    </q-table>
-  </div>
+        <template v-slot:item="props">
+          <div class="q-pa-xs col-xs-12 col-sm-6 col-md-4">
+            <q-card flat bordered class="cursor-pointer" @click="handleShowDetails(props.row)">
+              <q-img :src="props.row.img_url" :ratio="4/3" />
+              <q-card-section class="text-center">
+                <div class="text-h6">{{ props.row.name }}</div>
+                <div class="text-subtitle2">{{ formatCurrency(props.row.price) }}</div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </template>
+      </q-table>
+    </div>
+    <dialog-product-details
+      :show="showDialogDetails"
+      :product="productDetails"
+      @hide-dialog="showDialogDetails = false"
+    />
+  </q-page>
 </template>
 
 <script>
@@ -40,14 +47,20 @@ import useNotify from 'src/composables/UseNotify'
 import { useRoute } from 'vue-router'
 import { columnsProduct } from './table'
 import { formatCurrency } from 'src/utils/format'
+import DialogProductDetails from 'src/components/DialogProductDetails.vue'
 
 export default defineComponent({
   name: 'ProductListPublic',
+  components: {
+    DialogProductDetails
+  },
   setup () {
     const products = ref([])
     const loading = ref(true)
     const filter = ref('')
     const table = 'product'
+    const showDialogDetails = ref(false)
+    const productDetails = ref({})
 
     const { listPublic } = useApi()
     const { notifyError } = useNotify()
@@ -64,6 +77,11 @@ export default defineComponent({
       }
     }
 
+    const handleShowDetails = (product) => {
+      productDetails.value = product
+      showDialogDetails.value = true
+    }
+
     onMounted(() => {
       if (route.params.id) {
         handleListProducts(route.params.id)
@@ -75,7 +93,10 @@ export default defineComponent({
       products,
       loading,
       filter,
-      formatCurrency
+      formatCurrency,
+      showDialogDetails,
+      productDetails,
+      handleShowDetails
     }
   }
 })
