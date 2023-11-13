0c94ca44ae53e2bb32c509bceabeeffc07dde7ad
Neto Marçal <neto.marcal.dev@gmail.com>
2023-11-13 15:01:30 -0300

    Remoção relacionada ao Quasar no gitignire

---
+++ dist/spa/assets/UseNotify.ed29e0c6.js
@@ -0,0 +1,1 @@
+import{u as e}from"./use-quasar.b8b4cd8b.js";function i(){const s=e();return{notifySuccess:o=>{s.notify({type:"positive",message:o||"A\xE7\xE3o realizada com sucesso."})},notifyError:o=>{s.notify({type:"negative",message:o||"N\xE3o poss\xEDvel realizar a a\xE7\xE3o."})}}}export{i as u};
---
+++ dist/spa/assets/use-quasar.b8b4cd8b.js
@@ -0,0 +1,1 @@
+import{i as a,P as r}from"./index.5b155391.js";function u(){return a(r)}export{u};
