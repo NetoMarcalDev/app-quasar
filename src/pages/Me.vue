b6b0ff90fa90acfcf2067ffeeb2216a4b4c7e0c9
Neto Marçal <neto.marcal.dev@gmail.com>
2023-11-16 15:30:06 -0300

    Implementação do componente CardDashboard

--- src/pages/Me.vue
+++ src/pages/Me.vue
@@ -1,17 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
-    <div v-if="user">
+  <q-page padding>
+    <!--div v-if="user">
       <p>Olá, {{ user.email }}</p>
+    </div-->
+    <div class="row q-col-gutter-md">
+      <div class="col-md-4 col-xs-12 col-sm-6">
+        <card-dashboard
+          table='category'
+          label='Categorias'
+          icon='mdi-shape-outline'
+        />
+      </div>
+      <div class="col-md-4 col-xs-12 col-sm-6">
+        <card-dashboard
+          table='product'
+          label='Produtos'
+          icon='mdi-archive'
+        />
+      </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import useAuthUser from 'src/composables/UseAuthUser'
-import { defineComponent } from 'vue'
+import { defineComponent, defineAsyncComponent } from 'vue'
 
 export default defineComponent({
   name: 'MePage',
+  components: {
+    CardDashboard: defineAsyncComponent(() => import('components/CardDashboard.vue'))
+  },
   setup () {
     const { user } = useAuthUser()
 
